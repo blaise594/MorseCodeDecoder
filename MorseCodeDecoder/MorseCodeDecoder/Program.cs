e2971b8d7132e4b0665ec6dbbb9e48d5657c517a
blaise594 <danielbrogers594@gmail.com>
2017-03-29 17:09:18 -0400

    completed level 1

---
+++ MorseCodeDecoder/MorseCodeDecoder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCodeDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var path = "morse.csv";
+            Dictionary<string, string> morsecodedictionary = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(path))
+            {
+                
+                while (reader.Peek() > -1)
+                {
+                    var code = reader.ReadLine();
+                    Console.WriteLine(code);
+                    var split = code.Split(',');
+                    morsecodedictionary.Add(split[0], split[1]);
+
+
+                }
+
+            }
+            Console.WriteLine("Enter your message here.");
+            var input=Console.ReadLine().ToUpper();
+            foreach (var letter in input)
+            {
+                Console.WriteLine(morsecodedictionary[letter.ToString()]);
+            }
+
+        }
+    }
+}
